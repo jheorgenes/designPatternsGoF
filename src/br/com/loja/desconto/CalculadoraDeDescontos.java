1c97390154c5fdcccc56ea9475c79fe9a844b07d
Jheorgenes Warlley <jheorgenes@gmail.com>
2022-11-21 00:52:11 -0300

    [21/11/2022]   - Strategy   - Chain of Responsability   - Template Method   - State

---
+++ src/br/com/loja/desconto/CalculadoraDeDescontos.java
@@ -0,0 +1,17 @@
+package br.com.loja.desconto;
+
+import java.math.BigDecimal;
+
+import br.com.loja.orcamento.Orcamento;
+
+public class CalculadoraDeDescontos {
+
+	public BigDecimal calcular(Orcamento orcamento) {
+		Desconto cadeiaDeDescontos = new DescontoParaOrcamentoComMaisDeCincoItens(
+			new DescontoParaOrcamentoComValorMaiorQueQuinhentos(
+				new SemDesconto()	
+			)
+		);
+		return cadeiaDeDescontos.calcular(orcamento);
+	}
+}
